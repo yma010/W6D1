df029bdc16664897f65714f4cdb1372d60e67354
Marvin Ma <yma010@ucr.edu>
2019-08-05 15:50:05 -0700

    Knight Path found

--- skeleton/lib/knights.rb
+++ skeleton/lib/knights.rb
@@ -1,4 +1,4 @@
-require "00_tree_node.rb"
+require_relative "00_tree_node"
 
 class Knight_Pathfinder
 
@@ -13,8 +13,8 @@ class Knight_Pathfinder
     def build_move_tree
         seed = [@root_node]
         until seed.empty? 
-            leaf = seed.shift.value  
-            new_move_position(leaf).each do |branch|
+            leaf = seed.shift  
+            new_move_position(leaf.value).each do |branch|
                 new_leaf = PolyTreeNode.new(branch)
                 leaf.add_child(new_leaf)
                 seed << new_leaf
@@ -37,13 +37,31 @@ class Knight_Pathfinder
     def new_move_position(pos)
         arr = []
 
-        valid_m = self.valid_moves(pos)
+        valid_m = Knight_Pathfinder.valid_moves(pos)
         valid_m.each do |move| 
-            if !considered_positions.include?(move)
+            if !@considered_positions.include?(move)
                 arr << move
-                considered_positions << move
+                @considered_positions << move
             end
         end
         arr
     end
+
+    def find_path(end_position)
+        end_val = @root_node.dfs(end_position)
+        trace_path_back(end_val)
+    end
+
+    def trace_path_back(end_position)
+        empty_arr_path = [end_position.value]
+        until end_position.value == @root_node.value
+            end_position = end_position.parent
+            empty_arr_path << end_position.value
+        end
+        empty_arr_path.reverse!
+    end
 end
+
+knights = Knight_Pathfinder.new([0, 0])
+p knights.find_path([7, 6]) # => [[0, 0], [1, 2], [2, 4], [3, 6], [5, 5], [7, 6]]
+p knights.find_path([6, 2]) # => [[0, 0], [1, 2], [2, 0], [4, 1], [6, 2]]
